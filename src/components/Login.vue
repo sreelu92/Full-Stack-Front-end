6f76d9f7b8cc418ee3e86fc1ee10dfd819495d6c
sree <sreelu.ph@gmail.com>
2020-11-17 16:35:13 -0700

    started skeleton

---
+++ src/components/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h3>Username</h3>
+        <input v-model="username" type="text">
+        <h3>Password</h3>
+        <input v-model="password" type="text">
+        <button @click="login">Login</button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"login-page",
+        data() {
+            return {
+                username:"",
+                password:""
+            };
+        },
+        
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
